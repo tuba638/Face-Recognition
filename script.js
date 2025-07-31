b7303dbcd3656c51432e4e535ffd0c4980633357
tuba638 <tubatalib21@gmail.com>
2025-07-31 15:56:54 +0530

    update

--- script.js
+++ script.js
@@ -1,57 +1,58 @@
-const imageUpload = document.getElementById('imageUpload');
+const imageUpload = document.getElementById('imageUpload')
 
 Promise.all([
-    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-    faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
 ]).then(start)
 
 async function start() {
-    const container = document.createElement('div')
-    container.style.position = 'relative'
-    document.body.append(container)
-    const labeledFaceDescriptors = await loadLabeledImages()
-    const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6) //accept if 60% sure
-    let image
-    let canvas
-    document.body.append('Loaded')
-
-    imageUpload.addEventListener('change', async() => {
-        if (image) image.remove()
-        if (canvas) canvas.remove()
-        const image = await faceapi.bufferToImage(imageUpload.files[0])
-        container.append(image)
-        const canvas = faceapi.createCanvasFromMedia(image)
-        container.append(canvas)
-        const displaySize = { width: image.width, height: image.height }
-        faceapi.matchDimensions(canvas, displaySize)
-        const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
-
-        const resizedDetections= faceapi.resizeResults(detections, displaySize)
-        const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
-        results.forEach((result, i) => {
-            const box= resizedDetections[i].detection.box
-            const drawBox= new faceapi.draw.DrawBox(box, { label: result.toString() })
-            drawBox.draw(canvas)
-        })
+  const container = document.createElement('div')
+  container.style.position = 'relative'
+  document.body.append(container)
+  const labeledFaceDescriptors = await loadLabeledImages()
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.5)
+  let image
+  let canvas
+  document.body.append('Loaded')
+  imageUpload.addEventListener('change', async () => {
+    if (image) image.remove()
+    if (canvas) canvas.remove()
+    image = await faceapi.bufferToImage(imageUpload.files[0])
+    container.append(image)
+    canvas = faceapi.createCanvasFromMedia(image)
+    container.append(canvas)
+    const displaySize = { width: image.width, height: image.height }
+    faceapi.matchDimensions(canvas, displaySize)
+    const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
+    results.forEach((result, i) => {
+      const box = resizedDetections[i].detection.box
+      const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
+      drawBox.draw(canvas)
     })
-
-    function loadLabeledImages() {
-        const labels = []
-        return Promise.all(
-            labels.map(async label => {
-                const descriptions = ["Harry Potter", "Hermione Granger", "Ron Weasley", "Lord Voldemort", "Rubeus Hagrid", "Albus Dumbledore"]
-                for(let i=1 ; i<=2 ; i++){
-                    //2 images per persom: the more the merrier(better facial recognition)
-                    const img = await faceapi.fetchImage()
-                    const detections =  await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
-                    descriptions.push(detections.descriptor)
-                }
-                return new faceapi.LabeledFaceDescriptors(label, descriptions)
-
-            })
-        )
-    }
+  })
 }
+
+function loadLabeledImages() {
+  const labels = ['Harry Potter', 'Hermione Granger', 'Ron Weasley', 'Lord Voldemort', 'Rubeus Hagrid', 'Albus Dumbledore']
+  return Promise.all(
+    labels.map(async label => {
+      const descriptions = []
+      for (let i = 1; i <= 2; i++) {
+
+        const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/tuba638/Face-Recognition/refs/heads/main/labelled_images/Harry%20Potter/1.jpg`)
+
+        const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+        descriptions.push(detections.descriptor)
+      }
+
+      return new faceapi.LabeledFaceDescriptors(label, descriptions)
+    })
+  )
+}
+
+
 
 
